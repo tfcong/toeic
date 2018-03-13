8d0af13d7733a64303989b2de79cbbe9ef2e139f
Phạm Thành Công <congptph05286@fpt.edu.vn>
2018-03-13 16:13:37 +0700

    init project

---
+++ toeiconline-core-dao-impl/src/main/java/vn/itsol/core/daoimpl/RoleDaoImpl.java
@@ -0,0 +1,8 @@
+package vn.itsol.core.daoimpl;
+
+import vn.itsol.core.dao.RoleDao;
+import vn.itsol.core.data.daoimpl.AbstractDao;
+import vn.itsol.core.persistence.entity.RoleEntity;
+
+public class RoleDaoImpl extends AbstractDao<Integer,RoleEntity> implements RoleDao {
+}
---
+++ toeiconline-core-dao/src/main/java/vn/itsol/core/dao/RoleDao.java
@@ -0,0 +1,7 @@
+package vn.itsol.core.dao;
+
+import vn.itsol.core.data.dao.GenericDao;
+import vn.itsol.core.persistence.entity.RoleEntity;
+
+public interface RoleDao extends GenericDao<Integer,RoleEntity> {
+}
